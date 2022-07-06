71db4475422e2af3812caff41970c2ce2cf63a33
Muhammad Haseeb <haseebishtiaq300@gmail.com>
2022-07-06 18:04:00 +0500

    Intial Commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/recipe/:slug',
+    name: 'Recipe',
+    component: () => import('../views/Recipe.vue')
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
